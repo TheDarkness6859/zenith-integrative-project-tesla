9952d60d8573c01fec3949baa0b2354832c6a84c
juansebasmosquera519@gmail.com <juansebasmosquera519@gmal.com>
2026-02-15 18:49:25 -0500

    SCRUM-23 Refactor: fixing html information and adding js (fetch for the backend) in the app

--- frontend/static/js/auth.js
+++ frontend/static/js/auth.js
@@ -1,3 +1,6 @@
+import { loginForm, email, password, registerForm, registerName, registerEmail, registerPassword, confirmPassword} from "./elements.js";
+
+//interactive button for watch password
 function togglePassword(inputId, iconId) {
     const passwordInput = document.getElementById(inputId);
     const eyeIcon = document.getElementById(iconId);
@@ -33,4 +36,85 @@ if (toggleConfirm) {
     toggleConfirm.addEventListener("click", function () {
         togglePassword("confirmRegisterPassword", "confirmEyeIcon");
     });
+}
+
+
+
+// LOGIN
+
+if (loginForm) {
+    loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    try {
+    const response = await fetch("http://localhost:3000/api/auth/login", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+        email: email.value,
+        password: password.value
+        })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        localStorage.setItem("user", JSON.stringify(data.user));
+        localStorage.setItem("token", data.token);
+
+        alert("Login correcto");
+        window.location.href = "/dashboard";
+    } else {
+        alert(data.message || "Error al iniciar sesión");
+    }
+
+    } catch (error) {
+    console.error(error);
+    alert("No se pudo conectar al servidor");
+    }
+    });
+}
+
+
+// REGISTER
+
+if (registerForm) {
+    registerForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    // Validar contraseñas
+    if (registerPassword.value !== confirmPassword.value) {
+    alert("Las contraseñas no coinciden");
+    return;
+    }
+
+    try {
+    const response = await fetch("http://localhost:3000/api/auth/register", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+        name: registerName.value,
+        email: registerEmail.value,
+        password: registerPassword.value
+        })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        alert("Usuario registrado correctamente");
+        window.location.href = "/";
+    } else {
+        alert(data.message || "Error al registrar usuario");
+    }
+
+    } catch (error) {
+    console.error(error);
+    alert("No se pudo conectar al servidor");
+    }
+});
 }
---
+++ frontend/static/js/elements.js
@@ -0,0 +1,11 @@
+// LOGIN
+export const loginForm = document.getElementById("loginForm");
+export const email = document.getElementById("loginEmail");
+export const password = document.getElementById("loginPassword");
+
+// REGISTER
+export const registerForm = document.getElementById("registerForm");
+export const registerName = document.getElementById("registerName");
+export const registerEmail = document.getElementById("registerEmail");
+export const registerPassword = document.getElementById("registerPassword");
+export const confirmPassword = document.getElementById("confirmRegisterPassword");
