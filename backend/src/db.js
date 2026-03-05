6f2838b8bc6712aefdc0cfb1d0f0e45c6cd00aae
Esteban-Bustamante6 <estebanbl090@gmail.com>
2026-03-04 20:03:45 -0500

    only server

--- backend/src/db.js
+++ backend/src/db.js
@@ -9,7 +9,7 @@ dotenv.config({
 
 //Server connection
 
-/* const pool = new Pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
@@ -18,23 +18,23 @@ dotenv.config({
 
 pool.on("connect", (client) => {
   client.query(`SET search_path TO app_zenith, public`)
-});*/
+});
 
 //Local connection with .env
 
-const pool = new Pool({ 
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+// const pool = new Pool({ 
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
 
-pool.on("connect", (client) =>{
-  const schema = process.env.DB_SCHEMA || "public";
-  client.query(`SET search_path TO ${schema}, public`)
-    .catch(error => console.error("Error config search_path", error));
-}) 
+// pool.on("connect", (client) =>{
+//   const schema = process.env.DB_SCHEMA || "public";
+//   client.query(`SET search_path TO ${schema}, public`)
+//     .catch(error => console.error("Error config search_path", error));
+// }) 
 
 pool.query('SELECT NOW()', (err, res) => {
   if (err) {
